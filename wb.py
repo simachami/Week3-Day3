0f978a588c7265d5950419f5b51c97033b45822a
simachami <simabchami@gmail.com>
2023-06-28 22:18:47 -0600

    first commit for hw

---
+++ wb.py
@@ -0,0 +1,21 @@
+# Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+
+# Example
+# create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
+# The returned format must be correct in order to complete this challenge.
+
+# Don't forget the space after the closing parentheses!
+
+
+def create_phone_number(numbers):
+    # Convert the array of integers to a string
+    number_str = ''.join(str(num) for num in numbers)
+    
+    # Format the string as a phone number
+    formatted_number = "({}{}{}) {}{}{}-{}{}{}{}".format(*number_str)
+    
+    return formatted_number
+
+phone_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+result = create_phone_number(phone_numbers)
+print(result)
